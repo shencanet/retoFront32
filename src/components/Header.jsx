767766a7bf3269b803f9536aa1869f105c2fb5af
shen <santi.dionis@gmail.com>
2023-08-19 19:53:54 +0200

    ADD: Dark Mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,14 @@
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+        <h1>Social Media DashBoard</h1>
+        <p>Total Followers: 23,004</p>
+        <hr/>
+        <div className="flex place-content-between">
+            <p>Dark Mode</p>
+            <input type="checkbox" name="darkMode" id="darkMode"/>
+        </div>
+    </header>
   )
 }
