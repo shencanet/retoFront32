917a6029b25041506e9bc7c8c44de0cb810c7219
shen <santi.dionis@gmail.com>
2023-08-20 21:03:49 +0200

    Add: tailwind font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@
 
 export const Header = () => {
   return (
-    <header>
-        <h1>Social Media DashBoard</h1>
-        <p>Total Followers: 23,004</p>
-        <hr/>
-        <div className="flex place-content-between">
-            <p>Dark Mode</p>
+    <header className="bg-Very-Pale-Blue h-[235px] rounded-b-[20px] pt-8 px-6">
+        <h1 className="text-Very.Dark-Blue text-2x1 font-bold mb-1">Social Media DashBoard</h1>
+        <p className="text-Dark-Grayish-Blue font.bold mb-6">Total Followers: 23,004</p>
+        <hr className="bg-black mb[19px]" />
+        <div className="flex justify-between">
+            <p className="text-Dark-Grayish-Blue font-bold">Dark Mode</p>
             <input type="checkbox" name="darkMode" id="darkMode"/>
         </div>
     </header>
