2c4329c512cb636cf3df57c78b31884f549f0a02
shen <santi.dionis@gmail.com>
2023-08-26 16:22:04 +0200

    Add: tailwind Dark Mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,18 @@
-
+import { useState, useEffect } from "react";
 
 export const Header = () => {
-  const kandleClick = () => {
-    if (document.body.classList.contains("dark")) {
-      document.body.classList.remove("dark");
-      console.log("removido");
-    } else {
-      document.body.classList.add("dark");
-      console.log("agregado");
-    }
-
+ 
+  const [darkMode, setDarkMode] = useState(false);
+  const handleClick = () => {
+    setDarkMode(true);
   }
+  
+  useEffect(() => {
+    console.log("dark mode");
+  }, [darkMode])
+  
   return (
-    <header className="bg-Very-Pale-Blue h-[235px] rounded-b-[20px] pt-8 px-6">
+    <header className="bg-Very-Pale-Blue  dark:bg-red-600 h-[235px] rounded-b-[20px] pt-8 px-6">
         <h1 className="text-Very.Dark-Blue text-2x1 font-bold mb-1">Social Media DashBoard</h1>
         <p className="text-Dark-Grayish-Blue font-bold mb-6">Total Followers: 23,004</p>
         <hr className="bg-black mb-[19px]" />
@@ -21,7 +21,7 @@ export const Header = () => {
 
           <label htmlFor="darkMode" className="border  relative bg-Gray   w-[40px] h-[21px]  rounded-full cursor-pointer overflow-hidden
           p-[1px]">
-            <input onClick={kandleClick} type="checkbox" id="darkMode" className="peer sr-only" />
+            <input onClick={handleClick} type="checkbox" id="darkMode" className="peer sr-only" />
 
             <div className=" w-full  h-full peer-checked:bg-Toggle-Gradient absolute top-0 left-0 "></div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  export default {
-    darkMode: 'class',
-    content: ["./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",],
-    theme: {
-      extend: {
-        colors: {
-          "Lima-Green": "#8BC34A",
-          "Bright-Red": "#E53935",
-          "Facebook-Blue": "#3B5998",
-          "Twitter-Blue": "#1DA1F2",
-          "Youtube-Red": "#FF0000",
-          "Toggled-Gray": "#BDBDBD",
-          "Very-Light-Gray": "#F5F5F5",
-          "Light-Gray": "#E0E0E0",
-          "Gray": "#9E9E9E",
-          "Dark-Gray": "#616161",
-          "Very-Dark-Gray": "#212121",
-          "black": "#000000",
-          "White": "#FFFFFF",
-          "Toggle": "nsl(230, 22%, 74%)",
-          "Light-Grayish-Blue": "#ECEFF1",
-        },
-  },
-  content: ["./index.html",
-  "./src/**/*.{vue,js,ts,jsx,tsx}",],
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
@@ -42,25 +21,19 @@ export default {
         "Very-Dark-Gray": "#212121",
         "black": "#000000",
         "White": "#FFFFFF",
-        "Toggle": "nsl(230, 22%, 74%)",
+        "Toggle": "hsl(230, 22%, 74%)", // Corrected color format
         "Light-Grayish-Blue": "#ECEFF1",
-
-    },
-    backgroundImage:{
-      "Toggle-Gradient": "linear-gradient(to right, hsl(210, 78%, 56%), hsl(146, 68%, 55%))",
-      "Instagram": "linear-gradient(hsl(37, 97%, 70%), hsl(329, 70%, 58%))",
-
-
-    },
-   
+      },
+      backgroundImage: {
+        "Toggle-Gradient": "linear-gradient(to right, hsl(210, 78%, 56%), hsl(146, 68%, 55%))",
+        "Instagram": "linear-gradient(hsl(37, 97%, 70%), hsl(329, 70%, 58%))",
+      },
       fontFamily: {
-      
-      'Inter':['Inter'],
-      'sans': ['Roboto', 'sans-serif'],
-      'serif': ['Merriweather', 'serif'],
-      
-
+        'Inter': ['Inter'],
+        'sans': ['Roboto', 'sans-serif'],
+        'serif': ['Merriweather', 'serif'],
+      },
+    },
   },
   plugins: [],
-}
-  }}
+};
