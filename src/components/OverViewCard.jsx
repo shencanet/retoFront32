588b0f2bc32eaab3bc40cd560a90cc49790645e5
shen <santi.dionis@gmail.com>
2023-08-28 21:16:30 +0200

    Add: Component Logos

--- src/components/OverViewCard.jsx
+++ src/components/OverViewCard.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import facebookLogo from '../assets/images/icon-facebook.svg'
+import twitterLogo from '../assets/images/icon-twitter.svg'
+import instagramLogo from '../assets/images/icon-instagram.svg'
+import youtubeLogo from '../assets/images/icon-youtube.svg'
+
+const networkslogos = {
+  Facebook : facebookLogo,
+  Twitter : twitterLogo,
+  Instagram : instagramLogo,
+  Youtube : youtubeLogo
+
+}
+
+
 
 
 
 export const OverViewCard = ({user, audienceType, audience, network}) => {
   return (
     <article className='bg-Light-Grayish-Blue w-[326px] h-[216px] mb-4 rounded-[5px]  mx-auto'>
-        <img src={facebookLogo} alt={`logo ${network}`} className='w-[20px] h-[20px] mt-4 ml-4'/>
+        <img src={networkslogos[network]} alt={`logo ${network}`} className='w-[20px] h-[20px] mt-4 ml-4'/>
         <p>{user}</p>
         <p>{audience}</p>
         <p>{audienceType}</p>
